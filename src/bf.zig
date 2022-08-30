00c7cd5904fa18ee38c14894d8cb44796845c244
rubenkristian <rubenkristian96@gmail.com>
2022-08-31 00:03:34 +0700

    update bf.zig and struct

--- src/bf.zig
+++ src/bf.zig
@@ -3,42 +3,64 @@ const ArrayList = std.ArrayList;
 const Allocator = std.mem.Allocator;
 
 pub const Token = enum(u8) { NextPoint=1, PrevPoint=2, InValue=3, DeValue=4, Print=5, Scan=6, LoopOpen=7, LoopClose=8 };
-pub const PCODE = struct {
-  type: [3]u8,
-  value: u32,
-};
+// pub const PCODE = struct {
+//   type: [3]u8,
+//   value: u32,
+// };
 
 pub const Brainf = struct {
-  pointer: [1024]u8,
+  pointer: []*u8,
   allocator: Allocator,
   tokens: ArrayList(Token),
-  pcodes: ArrayList(PCODE),
-  currLoc: u16,
+  // pcodes: ArrayList(PCODE),
+  currLoc: usize,
+  stakeLoop: ArrayList(usize),
 
   pub fn init(allocator: Allocator) !Brainf {
     return Brainf{
-      .pointer = allocator.alloc(u8, 1024),
+      .pointer = try allocator.alloc(*u8, 64),
       .allocator = allocator,
       .tokens = ArrayList(Token).init(allocator),
-      .pcodes = ArrayList(PCODE).init(allocator),
+      .stakeLoop = ArrayList(usize).init(allocator),
+      // .pcodes = ArrayList(PCODE).init(allocator),
       .currLoc = 0,
     };
   }
 
   fn nextPointer(self: *Brainf) !void {
-    self.currLoc += 1;
+    if (self.currLoc < 63) {
+      self.currLoc += 1;
+    } else {
+      self.currLoc = 0;
+    }
   }
 
   fn prevPointer(self: *Brainf) !void {
-    self.currLoc -= 1;
+    if (self.currLoc > 0) {
+      self.currLoc -= 1;
+    } else {
+      self.currLoc = 63;
+    }
   }
 
   fn increaseCurrPointer(self: *Brainf) !void {
-    self.pointer[self.currLoc].* += 1;
+    if (self.pointer[self.currLoc].* < 255) {
+      self.pointer[self.currLoc].* += 1;
+    } else {
+      self.pointer[self.currLoc].* = 0;
+    }
   }
 
   fn decreaseCurrPointer(self: *Brainf) !void {
-    self.pointer[self.currLoc].* -= 1;
+    if (self.pointer[self.currLoc].* > 0) {
+      self.pointer[self.currLoc].* -= 1;
+    } else {
+      self.pointer[self.currLoc].* = 255;
+    }
+  }
+
+  fn currPointerZero(self: *Brainf) bool {
+    return self.pointer[self.currLoc].* == 0;
   }
 
   fn input(self: *Brainf) !void {
@@ -59,7 +81,7 @@ pub const Brainf = struct {
 
     var w = buf.writer();
 
-    try w.print("{c}", .{self.pointer[self.currLoc].*});
+    try w.print("{c}", .{self.pointer[self.currLoc]});
 
     try buf.flush();
   }
@@ -96,47 +118,95 @@ pub const Brainf = struct {
     return self.tokens.items;
   }
 
-  pub fn parse(self: *Brainf) !void {
-    var prevToken = undefined;
-    var countToken: u16 = 0;
-    var countLoop: u16 = 0;
-    for (self.tokens) |token| {
-      if (prevToken != undefined) {
-        if (prevToken == token) {
-          countToken += 1;
-        } else {
-          
-        }
-      } else {
-        prevToken = token;
-      }
-    }
-  }
+  // must check
+  // after allocation, value in
+  // pub fn reset(self: *Brainf) void {
+  //   for(self.pointer) |_, index| {
+  //     self.pointer[index] = 0;
+  //   }
+  // }
 
-  fn signPCode(count: u16, token: Token) !PCODE {
-    return PCODE {
-      .type=switch (token) {
-        Token.InValue => "inc",
-        Token.DeValue => "dec",
-        Token.NextPoint => "nex",
-        Token.PrevPoint => "pre",
-        Token.Print => "pri",
-        Token.Scan => "scn",
-        Token.LoopOpen => "lop",
-        Token.LoopClose => "lcl"
-      },
-      .value=count,
-    };
-  }
+  // pub fn parse(self: *Brainf) !void {
+  //   var prevToken = undefined;
+  //   var countToken: u16 = 0;
+  //   var countLoop: u16 = 0;
+  //   for (self.tokens) |token| {
+  //     if (prevToken != undefined) {
+  //       if (prevToken == token) {
+  //         countToken += 1;
+  //       } else {
+          
+  //       }
+  //     } else {
+  //       prevToken = token;
+  //     }
+  //   }
+  // }
+
+  // fn signPCode(count: u16, token: Token) !PCODE {
+  //   return PCODE {
+  //     .type=switch (token) {
+  //       Token.InValue => "inc",
+  //       Token.DeValue => "dec",
+  //       Token.NextPoint => "nex",
+  //       Token.PrevPoint => "pre",
+  //       Token.Print => "pri",
+  //       Token.Scan => "scn",
+  //       Token.LoopOpen => "lop",
+  //       Token.LoopClose => "lcl"
+  //     },
+  //     .value=count,
+  //   };
+  // }
 
   pub fn Interpreter(self: *Brainf) !void {
-    for (self.tokens) |token| {
-
+    const maxRows = self.tokens.items.len;
+    var index:usize = 0;
+    while (index < maxRows) {
+      const token = self.tokens.items[index];
+      
+      switch (token) {
+        .InValue => {
+          try self.increaseCurrPointer();
+          index += 1;
+        },
+        .DeValue => {
+          try self.decreaseCurrPointer();
+          index += 1;
+        },
+        .NextPoint => {
+          try self.nextPointer();
+          index += 1;
+        },
+        .PrevPoint => {
+          try self.prevPointer();
+          index += 1;
+        },
+        .Print => {
+          try self.writeBuffer();
+          index += 1;
+        },
+        .Scan => {
+          index += 1;
+        },
+        .LoopOpen => {
+          try self.stakeLoop.append(index);
+          index += 1;
+        },
+        .LoopClose => {
+          if (self.currPointerZero()) {
+            _ = self.stakeLoop.pop();
+            index += 1;
+          } else {
+            index = self.stakeLoop.pop();
+          }
+        },
+      }
     }
   }
 
   pub fn deinit(self: *Brainf) void{
     self.tokens.deinit();
-    self.pcodes.deinit();
+    // self.pcodes.deinit();
   }
 };
